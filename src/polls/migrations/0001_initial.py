b12d3762f30ce176ac88d18b55e050dbe2015fb2
Gordon Cassie <gordon.cassie@imanage.com>
2021-10-30 12:45:03 -0700

    Step 8

--- src/polls/migrations/0001_initial.py
+++ src/polls/migrations/0001_initial.py
@@ -1,5 +1,5 @@
-from django.db import migrations, models
 import django.db.models.deletion
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -12,7 +12,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="Question",
             fields=[
-                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
                 ("question_text", models.CharField(max_length=200)),
                 ("pub_date", models.DateTimeField(verbose_name="date published")),
             ],
@@ -20,10 +28,23 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="Choice",
             fields=[
-                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
                 ("choice_text", models.CharField(max_length=200)),
                 ("votes", models.IntegerField(default=0)),
-                ("question", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="polls.question")),
+                (
+                    "question",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="polls.question"
+                    ),
+                ),
             ],
         ),
     ]
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-
-from .models import Question, Choice
+from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
