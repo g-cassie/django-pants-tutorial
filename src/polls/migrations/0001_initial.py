574a9f3101349d5f71365800d085168061776844
Gordon Cassie <gordon.cassie@imanage.com>
2021-11-16 12:13:17 -0800

    Fix conflicting configurations for black and isort.

--- src/polls/migrations/0001_initial.py
+++ src/polls/migrations/0001_initial.py
@@ -41,9 +41,7 @@ class Migration(migrations.Migration):
                 ("votes", models.IntegerField(default=0)),
                 (
                     "question",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE, to="polls.question"
-                    ),
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="polls.question"),
                 ),
             ],
         ),
